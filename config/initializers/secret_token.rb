89f9a2a3d815e57f882c55094f5a9aa36f9f6691
Samuel Leach <samuel.leach@gmail.com>
2013-04-19 15:01:06 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graphcache::Application.config.secret_token = '8c2ff08749fb05f05746a1105d402c739f7202f29597533a1d210194ad9fc867a8fe3fd2d15a5214da3cd6881c374da440c44adbbd00be07c33352633c3fda8a'
