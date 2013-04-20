faa8776633bd51a6ed201960475e69013982dc85
Samuel Leach <samuel.leach@gmail.com>
2013-04-20 18:15:49 +0100

    Adding prototype D3 force directed graph view

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,11 +1,12 @@
 class GraphsController < ApplicationController
 
-
 def show
+	@links = Link.all
+	@nodes = Node.all
 
-   respond_to do |format|
-      format.html # show.html.erb
-   end
+   	respond_to do |format|
+    	format.html # show.html.erb
+   	end
 end
 
 
