d42915c001bb4934e0d4f5d8867e3c169b4f19e6
Samuel Leach <samuel.leach@gmail.com>
2013-04-21 15:04:54 +0100

    Adding validators to models

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,7 @@ class Link < ActiveRecord::Base
   attr_accessible :source_id, :strength, :target_id
 
   validates :source_id, :target_id, :presence => true
+#  validates_uniqueness_of :source_id, :scope => :target_id # Violation of this validation gives a slightly confusing error message
+  validates :source_id, :uniqueness => { :scope => :target_id }
+
 end
--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,6 @@
 class Node < ActiveRecord::Base
   attr_accessible :created_at, :description, :followers_count, :friends_count, :group_id, :id, :location, :name, :profile_image_url_https, :protected, :statuses_count
+
+  validates :id, :presence => true
+  validates :id, :uniqueness => true
 end
