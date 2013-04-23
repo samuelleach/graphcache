62074f30382b621c4aa971ecb7a78477dac4fe64
Samuel Leach <samuel.leach@gmail.com>
2013-04-23 15:56:28 +0100

    Fixing bug: provide -> provider

--- db/migrate/20130423091501_create_users.rb
+++ db/migrate/20130423091501_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :provide
+      t.string :provider
       t.string :uid
       t.string :name
 
