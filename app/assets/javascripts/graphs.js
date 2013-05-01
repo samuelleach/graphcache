49bcf11cdffcea2be23bf0ea4830c21612669f32
Samuel Leach <samuel.leach@gmail.com>
2013-05-01 21:02:19 +0100

    Updating graph view

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,96 +1,192 @@
     d3.json("/graphs/d3data.json", function(error, json) {
       if (error) return console.warn(error);
 
+      var opacity = 0.1;
+
       nodes = json.nodes;
       links = json.links;
 
-      var myMouseOverFunction = function(d) {
+      var mouseOverFunction = function(d) {
+    
+        infobox
+          .style("display", "block")
+          .select("p")
+          .text("Number of followers: " + d.size + ", Twitter ID: " + d.id + ", Name: " + d.name)
+          .style("opacity", 0)
+          .transition()
+            .style("opacity", 1)
+          .duration(500);
+
+        node
+          .transition(500)
+            .style("opacity", function(o) {
+              return isConnected(o, d) ? 1 : opacity;
+            })
+          .style("fill", function(o) {
+            if (isConnectedAsTarget(o, d) && isConnectedAsSource(o, d) ) {
+              fillcolor = 'green';
+            } else if (isConnectedAsSource(o, d)) {
+              fillcolor = 'red';
+            } else if (isConnectedAsTarget(o, d)) {
+              fillcolor = 'blue';
+            } else if (isEqual(o, d)) {
+              fillcolor = "hotpink";
+            } else {
+              fillcolor = "black";
+            }
+            return fillcolor;
+          });
+
+        link
+          .transition()
+            .style("stroke-opacity", function(o) {
+              return o.source === d || o.target === d ? 1 : opacity;
+            })
+          .style("stroke-width",2.0)
+          // .attr("marker-end", function(o) {
+          //     return o.source === d || o.target === d ? "url(#arrowhead)" : "url()";
+          // })
+          .duration(500);
+
         var circle = d3.select(this);
-        //circle.attr("fill", "red");
 
-        d3.select(".infobox").style("display", "block");  
-        d3.select("p").text("Number of followers: " + d.size +
-                            ", Twitter ID: "+ d.id + 
-                            ", Name: "+d.name);
-
-        d3.select("p").style("opacity", 0)
-                      .transition()
-                      .style("opacity", 1).duration(500);
+        circle
+          .transition()
+            .attr("r", function(){ return 1.2 * node_radius(d)})
+          .duration(500);
       }
 
-      var myMouseOutFunction = function() {
+      var mouseOutFunction = function() {
         var circle = d3.select(this);
-       // circle.attr("fill", "steelblue" );
         
-        d3.select(".infobox").style("display", "none"); 
+        infobox
+          .style("display", "none"); 
+
+        // node
+        //   .transition().style("opacity", 1).duration(500);
+
+        link
+          .transition()
+            // .attr("marker-end","url()")
+            // .style("stroke-opacity", 1)
+            // .style("stroke-width",0.8)
+          // .duration(500);
+
+        circle
+          .transition()
+            .attr("r", node_radius)
+          .duration(500);
       }
 
-      var myMouseMoveFunction = function() {
-        var infobox = d3.select(".infobox");
+      var mouseMoveFunction = function() {
         var coord = d3.mouse(this)
-        infobox.style("left", coord[0] + 105  + "px" );
-        infobox.style("top", coord[1] + 30 + "px");
+        infobox
+          .style("left", coord[0] + 105  + "px" )
+          .style("top", coord[1] + 30 + "px");
       }
 
-      var w = 800; 
-      var h = 400;
+      function zoom() {
+          svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+      }
 
-      vis = d3.select("#graphcontainer").append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .on('mousemove', myMouseMoveFunction),
-      
+      function tick() {
+        link
+          .attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+
+        node
+          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      }
+
+      function node_radius(d) {
+        return Math.pow(30.0 * d.weight, 1/3);
+      }
+
+      function isConnected(a, b) {
+          return isConnectedAsTarget(a, b) || isConnectedAsSource(a, b) || isEqual(a,b);
+      }
+
+      function isConnectedAsSource(a, b) {
+          return linkedByIndex[a.index + "," + b.index];
+      }
+
+      function isConnectedAsTarget(a, b) {
+          return linkedByIndex[b.index + "," + a.index];
+      }
+
+      function isEqual(a, b) {
+          return a.index == b.index;
+      }
+
+
+      var w = 1000; 
+      var h = 500;
+
       force = d3.layout.force()
         .nodes(nodes)
         .links(links)
         .charge(-1500)
-        .friction(0.8)
+        .friction(0.6)
         .gravity(0.9)
         .size([w,h])
-        .start(),
-              
-      link = vis.selectAll("line")
-        .data(links)
-        .enter()
-          .append("line")
-          .attr("class","link"),
-          
-      node = vis.selectAll(".node")
-        .data(nodes)
-        .enter()
-          .append("g")
-          .attr("class","node")
-          .call(force.drag);
+        .start();
 
-      fill = d3.scale.category20(),
-
-      node.append("circle")
-        .attr("r", function(d) {
-          return Math.pow(40*d.size,1/3);
-          })
-        .attr("fill",function(d) {
-          return fill(d.size);
-          })
-        .attr("stroke","black")
-        .attr("stroke-width",2)
-        .on("mouseover", myMouseOverFunction)
-        .on("mouseout", myMouseOutFunction),
-  
-        // node.append("text")
-        //   .attr("dx",function(d) {
-        //     return Math.pow(40*d.size,1/3)+1;
-        //     })
-        //   .attr("dy",".35em")
-        //   .text(function(d) {return d.atom;});
-        
-      force.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
-          
-      node.attr("transform", function(d) { 
-          return "translate(" + d.x + "," + d.y + ")"; 
-          });   
+      // Place this after the d3.layout.force()
+      var linkedByIndex = {};
+      links.forEach(function(d) {
+          linkedByIndex[d.source.index + "," + d.target.index] = 1;
       });
+
+      force.on("tick",tick);
+
+      var svg = d3.select('#graphcontainer').append('svg')
+                  .attr('width', w)
+                  .attr('height', h)
+                .on('mousemove', mouseMoveFunction)
+                .append("g")
+                .call(d3.behavior.zoom().scaleExtent([0.5, 8]).on("zoom", zoom))
+                .append('g');
+
+      var link = svg.selectAll("line")
+                    .data(links)
+                  .enter().append("line");
+
+      var node = svg.selectAll(".node")
+                    .data(nodes)
+                  .enter().append("g")
+                    .attr("class","node")
+                    .call(force.drag);
+
+      var fill = d3.scale.category20();
+
+      svg
+        .append("marker")
+        .attr("id", "arrowhead")
+        .attr("refX", 6 + 3) // Controls the shift of the arrow head along the path
+        .attr("refY", 2)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 4)
+        .attr("orient", "auto")
+        .append("path")
+          .attr("d", "M 0,0 V 4 L6,2 Z");
+
+      node
+        .append("circle")
+        .attr("r", node_radius)
+        .attr("stroke","white")
+        .attr("stroke-width",2.0)
+        .on("mouseover", mouseOverFunction)
+        .on("mouseout", mouseOutFunction);
+
+      link
+        .attr("class","link")
+        .style("stroke-width",1);
+
+      link
+        .attr("marker-end", "url()");
+
+      var infobox = d3.select('.infobox');
+
   });
--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -13,61 +13,121 @@ end
 
 
 def d3data
-	@links = Link.all
+	@links_raw = Link.all
 	@nodes = Node.all
 
-	@nodeslookup = {}
-	@d3links = []
+	# Perform and initial filter of the data to remove nodes with only one edge
+	@nodes_lookup = {}
+	@links_raw.each do |link|
+		link_source_id = link[:source_id]
+		link_target_id = link[:target_id]
+		if @nodes_lookup[link_source_id] == nil
+			@nodes_lookup[link_source_id] = 1
+		else
+			@nodes_lookup[link_source_id] += 1
+		end
+		if @nodes_lookup[link_target_id] == nil
+			@nodes_lookup[link_target_id] = 1
+		else
+			@nodes_lookup[link_target_id] += 1
+		end
+	end
+	@links = []
+	@links_raw.each do |link|
+		if @nodes_lookup[link[:source_id]] > 2 && @nodes_lookup[link[:target_id]] > 2
+			@links << link
+		end
+	end
 
 	# Reform graph links data for d3
+	@nodeslookup = {}
+	@d3links = []
 	counter = -1
 	@links.each do |link|
 		tmplink = {}
-		if @nodeslookup[link[:source_id]] == nil
+
+		link_id = link[:source_id]
+		link_key = :source
+		if @nodeslookup[link_id] == nil
 			counter += 1
-			tmplink[:source] = counter
-			@nodeslookup[link[:source_id]] = counter
+			tmplink[link_key] = counter
+			@nodeslookup[link_id] = { node_id: counter, num_links_as_source: 1, num_links_as_target: 0 }
 		else
-			tmplink[:source] = @nodeslookup[link[:source_id]]
+			tmplink[link_key] = @nodeslookup[link_id][:node_id]
+			@nodeslookup[link_id][:num_links_as_source] += 1
 		end
 
-		if @nodeslookup[link[:target_id]] == nil
+		link_id = link[:target_id]
+		link_key = :target
+		if @nodeslookup[link_id] == nil
 			counter += 1
-			tmplink[:target] = counter
-			@nodeslookup[link[:target_id]] = counter
+			tmplink[link_key] = counter
+			@nodeslookup[link_id] = { node_id: counter, num_links_as_target: 1, num_links_as_source: 0 }
 		else
-			tmplink[:target] = @nodeslookup[link[:target_id]]
+			tmplink[link_key] = @nodeslookup[link_id][:node_id]
+			@nodeslookup[link_id][:num_links_as_target] += 1
 		end
 		@d3links << tmplink
 	end
 
-	# Extracting data about the nodes - need to perform checks and validation
+	# Extracting data about the nodes
 	@d3nodes = []
 	@nodeslookup.each do |key, value|
 		id = key
+		nodeinfo = value
 		begin
 			@node = Node.find(id)
 			count = @node[:followers_count]
+			name = @node[:name]
 		rescue
-			count = 1 # Need to edit this so that we provide info that there is no node info
+			count = nodeinfo[:num_links_as_target]
+			name = ''
 		end
-		@d3nodes <<  {size: count, id: id}#, name: @node[:name] }
+
+		@d3nodes <<  {size: count, id: id , name: name,
+					 num_links_as_source: nodeinfo[:num_links_as_source],
+					 num_links_as_target: nodeinfo[:num_links_as_target] }
 	end
 
+
+	# Apply a weight to each link. link weight = 0 if it is attached to a node
+	# with only one edge, else the weight = 1.
+	i = -1
+	@d3links.each do |link|
+		i += 1
+		source_node = @d3nodes[link[:source]]
+		target_node = @d3nodes[link[:target]]
+
+		if source_node[:num_links_as_source] + source_node[:num_links_as_target] == 1 ||
+			target_node[:num_links_as_source] + target_node[:num_links_as_target] == 1
+			link[:weight] = 0
+		else
+			link[:weight] = 1
+		end
+	end
+
+
+
    	respond_to do |format|
-    	format.json { render json: {"nodes" => @d3nodes, "links" => @d3links }}#, "lookup" => @nodeslookup} }
+    	format.json { render json: {"nodes" => @d3nodes, "links" => @d3links } }#, "test" => @nodeslookup_new } }
    	end
 end
 
 def friends
 
 	screen_name = params[:screen_name]
-	user = Twitter.user(screen_name.to_s)
+	user = Twitter.user(screen_name)
 	# render :json => user
 
 	cursor = -1
 	ids = twitter_ids(user.id, "friends", cursor)
 	# render :json => ids
+
+	if ids.attrs[:next_cursor] == 0
+		node = Node.find_by_id(user.id) || Node.create(:id => user.id)
+		node = Node.update(user.id, :friends_count => ids.attrs[:ids].length)
+		node = Node.update(user.id, :name => user.name)
+	end
 
 	ids.each do |id|
 		link = Link.create(:source_id => user.id, :target_id => id)
@@ -79,10 +139,17 @@ end
 def followers
 
 	screen_name = params[:screen_name]
-	user = Twitter.user(screen_name.to_s)
+	user = Twitter.user(screen_name)
 
 	cursor = -1
 	ids = twitter_ids(user.id, "followers", cursor)
+	# pry.instance
+
+	if ids.attrs[:next_cursor] == 0
+		node = Node.find_by_id(user.id) || Node.create(:id => user.id)
+		node = Node.update(user.id, :followers_count => ids.attrs[:ids].length)
+		node = Node.update(user.id, :name => user.name)
+	end
 
 	ids.each do |id|
 		link = Link.create(:source_id => id, :target_id => user.id)
